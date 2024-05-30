2b8bbd4183cebdf5603540a0163dc3f7be5b4942
Abhiram-044 <109068782+Abhiram-044@users.noreply.github.com>
2024-05-30 08:44:23 +0530

    Added menu.py for nav bar

--- menu.py
+++ menu.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 def authenticated_menu():
-    st.sidebar.page_link("main", label="Switch Accounts")
-    st.sidebar.page_link("pages/user_shows", label="Your profile")
-    st.sidebar.page_link("pages/top", label="Top")
-    st.sidebar.page_link("pages/search", label="Search")
+    st.sidebar.page_link("pages/user_shows.py", label="Your profile")
+    st.sidebar.page_link("pages/top.py", label="Top")
+    st.sidebar.page_link("pages/search.py", label="Search")
+    st.sidebar.button("Logout", on_click=lambda: st.session_state.update({"logged_in": False}) and st.rerun(), key="Logout")
 
 def unauthenticated_menu():
-    st.sidebar.page_link("main", label="Log in")
+    st.sidebar.page_link("app.py", label="Log in")
 
 def menu():
     if "logged_in" not in st.session_state or not st.session_state.logged_in:
@@ -17,5 +17,5 @@ def menu():
 
 def menu_with_redirect():
     if "logged_in" not in st.session_state or not st.session_state.logged_in:
-        st.session_state.current_page = "main"
+        st.session_state.current_page = "app.py"
     menu()
